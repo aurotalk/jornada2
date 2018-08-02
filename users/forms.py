04d92db0ed9d785e86fd1077571c273244189311
jornadaciti <jornadaciti@ug4c16.windows.cin.ufpe.br>
2018-08-02 17:44:21 -0300

    last dia

--- users/forms.py
+++ users/forms.py
@@ -9,3 +9,7 @@ class UserSignupForm(forms.ModelForm):
         fields = ('username', 'email')
     def clean_email(self):
         email = self.cleaned_data.get('email')
+        user_exists = User.objects.filter(email=email).exists()
+        if user_exists :
+            raise
+        return email
